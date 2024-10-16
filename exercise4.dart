330e5351a7f6c0816276bc4a73cd6908f0dcbda7
Albert <albert182219@gmail.com>
2024-10-16 16:16:03 -0400

    Commit4

--- exercise4.dart
+++ exercise4.dart
@@ -1,3 +1,46 @@
-// 4. Ejercicio 4: Extensiones y Enumeraciones
+// 4. Ejercicio 4: Extensiones y Enumeraciones.
 //  Crea una enumeración DiaSemana con los días de la semana.
 //  Define una extensión sobre int que devuelva true si el número es par. Úsalo para verificar si un número ingresado por el usuario es par.
+
+
+
+import 'dart:io';
+
+enum DiaSemana {
+  lunes,
+  martes,
+  miercoles,
+  jueves,
+  viernes,
+  sabado,
+  domingo
+}
+
+
+extension NumeroPar on int {
+  bool esPar() {
+    return this % 2 == 0;
+  }
+}
+
+void main() {
+ 
+  print('Días de la semana:');
+  for (var dia in DiaSemana.values) {
+    print(dia);
+  }
+
+  print('\nIngresa un número para verificar si es par:');
+  int? numero = int.tryParse(stdin.readLineSync()!);
+
+  
+  if (numero != null) {
+    if (numero.esPar()) {
+      print('El número $numero es par.');
+    } else {
+      print('El número $numero es impar.');
+    }
+  } else {
+    print('Por favor, ingresa un número válido.');
+  }
+}
