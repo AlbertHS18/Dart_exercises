f9be6061efd9f6c317270ba063e64d8ebf8172d2
Albert <albert182219@gmail.com>
2024-10-16 15:58:14 -0400

    Commit1

--- exercise1.dart
+++ exercise1.dart
@@ -1,3 +1,30 @@
-//  1. Ejercicio 1: Clases y Objetos
+//  1. Ejercicio 1: Clases y Objetos.
 //  Define una clase Persona con atributos, nombre y edad. Crea una instancia de esta clase e imprime sus valores.
 //  Agrega un método para mostrar los detalles de la persona.
+
+
+class Persona {
+  
+  String nombre;
+  int edad;
+
+
+  Persona(this.nombre, this.edad);
+
+ 
+  void mostrarDetalles() {
+    print('Nombre: $nombre, Edad: $edad años');
+  }
+}
+
+void main() {
+  
+  var persona1 = Persona('Albert', 23);
+
+ 
+  print('Nombre: ${persona1.nombre}');
+  print('Edad: ${persona1.edad} años');
+
+ 
+  persona1.mostrarDetalles();
+}
